5c379f2c2dc2113fcdfb6d6e3cf413d71284c1db
Benjamin Dally <benjamin.dally@icloud.com>
2018-04-14 13:45:55 -0700

    fixed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,22 +21,22 @@ var characters = [
     name: "Yoda",
     role: "Jedi Master",
     age: 900,
-    forcePoints: 2000
+    forcePoints: 2000,
   },
   {
     routeName: "darthmaul",
     name: "Darth Maul",
     role: "Sith Lord",
     age: 200,
-    forcePoints: 1200
+    forcePoints: 1200,
   },
   {
     routeName: "obiwankenobi",
     name: "Obi Wan Kenobi",
     role: "Jedi Master",
     age: 55,
-    forcePoints: 1350
-  }
+    forcePoints: 1350,
+  },
 ];
 
 // Routes
